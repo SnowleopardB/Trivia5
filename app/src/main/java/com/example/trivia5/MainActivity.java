f716e025986f187234fe7cac3a3d72ecccec9056
SnowleopardB <b.j.g.akerboom@gmail.com>
2019-06-04 18:47:12 +0200

    finished but not clean

--- app/src/main/java/com/example/trivia5/MainActivity.java
+++ app/src/main/java/com/example/trivia5/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.trivia5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements QuestionsRequest.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartOnClick(View view) {
+        QuestionsRequest request = new QuestionsRequest(this);
+        request.getQuestions( this);
+    }
+
+    @Override
+    public void gotQuestionlist(ArrayList<Question> someQuestions) {
+        Intent intent = new Intent(MainActivity.this, QuestionsActivity.class);
+        intent.putExtra("questions", someQuestions);
+        startActivity(intent);
+    }
+
+    @Override
+    public void gotQuestionlistError(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
